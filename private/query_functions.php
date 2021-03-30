a8f738cd50e83f5937b73a634b292727f6386afb
Shane Maxson <smaxson0927@gmail.com>
2021-03-30 08:40:49 -0400

    Completed Asgn08

---
+++ private/query_functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function find_all_subjects() {
+  global $db;
+  $sql = "SELECT * FROM subjects ";
+  $sql .= "ORDER BY position ASC";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  return $result;
+}
+
+function find_all_pages() {
+  global $db;
+  $sql = "SELECT * FROM pages ";
+  $sql .= "ORDER BY subject_id ASC, position ASC";
+  $result = mysqli_query($db, $sql);
+  confirm_result_set($result);
+  return $result;
+}
+
+?>
